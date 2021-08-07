e2ecc775c798944938b95557f2b76a56a1b1ccc4
XiaoJiang <2841067313@qq.com>
2021-08-07 16:02:14 +0800

    firstCommitGit

---
+++ src/main/GitTest.java
@@ -0,0 +1,11 @@
+package main;
+/*
+@author Jiang Wei
+@create 2021-08-07-15:59
+*/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
